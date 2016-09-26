f2928139255aa5c6ef731dce34e4f73eb1890fe0
Bang Nguyen <bangnv77@yahoo.com>
2016-09-25 20:57:19 -0700

    add publication date for each post

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,9 @@ class Post < ApplicationRecord
   def should_generate_new_friendly_id?
     title_changed?
   end
+
+  def display_day_published
+    "Published #{created_at.strftime('%-b %-d, %Y')}"
+  end
+
 end
